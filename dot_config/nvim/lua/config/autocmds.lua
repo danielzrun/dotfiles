434f30af1c1c56f76585ba9f94338993aa10b263
daniel <daniel123.xiang@gmail.com>
2026-01-08 23:13:56 +0800

    refactor: reorganize darwin automation scripts with run_once prefix and numeric ordering

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function()
+        -- Only copy to system clipboard when the operation is "yank" (y)
+        -- Exclude delete (d), change (c), and other operations
+        if vim.v.event.operator == "y" then
+            vim.fn.setreg("+", vim.fn.getreg("0"))
+        end
+    end,
+})
