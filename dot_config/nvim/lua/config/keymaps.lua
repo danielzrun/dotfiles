ab10f26c1cf3757548e054a9db68c17a08f155c4
Daniel <daniel123.xiang@gmail.com>
2026-01-16 19:04:04 +0800

    feat: enhance nvim config with ui tweaks and root detection
    - Set root_spec priority to cwd first for proper project detection
    - Add scroll wheel viewport scroll mappings
    - Enable mouse support and scrolloff=8
    - Configure clipboard without unnamedplus (use + register explicitly)

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -4,8 +4,16 @@
 
 -- Copy to system clipboard in visual mode using Cmd+C
 vim.keymap.set("v", "<D-c>", '"+y', { desc = "Copy to system clipboard" })
+vim.keymap.set("n", "y", '"+y', { desc = "Yank to system clipboard" })
+vim.keymap.set("n", "yy", '"+yy', { desc = "Yank line to system clipboard" })
+vim.keymap.set("v", "y", '"+y', { desc = "Yank to system clipboard" })
+
 
 -- Paste from system clipboard using Cmd+V in normal and visual mode
 vim.keymap.set({ "n", "v" }, "<D-v>", '"+p', { desc = "Paste from system clipboard" })
 -- Paste from system clipboard using Cmd+V in insert mode
 vim.keymap.set("i", "<D-v>", "<C-r>+", { desc = "Paste from system clipboard" })
+
+-- Scroll viewport without moving cursor (scroll wheel behavior)
+vim.keymap.set({ "n", "v" }, "<ScrollWheelUp>", "<C-y>", { silent = true, desc = "Scroll viewport up" })
+vim.keymap.set({ "n", "v" }, "<ScrollWheelDown>", "<C-e>", { silent = true, desc = "Scroll viewport down" })
--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -7,5 +7,14 @@ vim.opt.spell = false
 -- Configure spell checking languages for English and CJK (when enabled)
 vim.opt.spelllang = { "en", "cjk" }
 
--- Disable clipboard integration to prevent conflicts with system clipboard keymaps
+-- Set root directory priority (cwd first, then lsp, then .git)
+vim.g.root_spec = { "cwd", "lsp", ".git" }
+
+-- Disable clipboard integration (use "+ register explicitly for system clipboard)
 vim.opt.clipboard = ""
+
+-- Enable mouse support
+vim.opt.mouse = "a"
+
+-- Keep 8 lines visible when scrolling
+vim.opt.scrolloff = 8
